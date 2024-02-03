48d3c364b66041b5f54b78ff63247ec40d4eed54
jeniels <swatimehta744@gmail.com>
2024-02-03 13:37:43 +0530

    backend added

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
+
+
+app.get('/',(req,res)=>
+{
+    res.send("Hello World");
+})
